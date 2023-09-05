6e1d109b284321e7c990ed13221b952751e7968e
HassanShafiq97 <hassanshafiqe97@gmail.com>
2023-09-04 23:40:19 -0700

    login done

---
+++ database/seeders/User_Seeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
+class User_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name'=>'hassan shafique',
+            "email"=>"hassanshafiqe97@gmail.com",
+            "password"=>Hash::make('123456')
+
+        ]);  
+    }
+}
